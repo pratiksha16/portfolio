b459c7b09df5b04f4d5b38cd31c0129c7380f113
pratiksha16 <pratiksha.adhikari@wmich.edu>
2020-08-08 17:52:48 -0400

    Updates

---
+++ precache-manifest.3c0b69d9f312f298319bc79a70c8e0b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e797c47bb5f1211ae6f1061b15cbc81",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0027d9022d14c7d89912",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0027d9022d14c7d89912",
+    "url": "/portfolio/static/js/main.2f1ba889.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
