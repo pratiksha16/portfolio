728b3b59717e72da4860e1cb92886bc7d453e53b
pratiksha16 <pratiksha.adhikari@wmich.edu>
2020-08-09 14:01:27 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.32dfd6e86ae992668d7e0fd488edf8f6.js"
+  "/portfolio/precache-manifest.8ff05f88b221d3552edb6b42b1e164d9.js"
 );
 
 self.addEventListener('message', (event) => {
